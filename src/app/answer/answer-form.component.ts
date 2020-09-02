37172b77ea8531bc9645b23a936e404bbd8a42c2
Mr Garrix RL <rlfernando7@gmail.com>
2020-09-02 18:44:07 -0500

    Implement code to add each answer we create in the viewport

--- src/app/answer/answer-form.component.ts
+++ src/app/answer/answer-form.component.ts
@@ -1,6 +1,8 @@
 //Create a ts component for my answer-form component
-import { Component } from '@angular/core';
+import { Component,Input } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Answer,User } from './answer.model';
+import { Question } from '../question/question.model';
 
 //Indicate where can I find the template, which selector I will use and the css
 @Component({
@@ -9,8 +11,20 @@ import { NgForm } from '@angular/forms';
 	styleUrls: ['./answer-form.component.css']
 })
 export class AnswerFormComponent{
+	//Con Input indico que es una propiedad que se le va a pasar
+	@Input() question: Question;
 	//Call my component answer(onSubmit) and send parameters (create an object)
 	onSubmit(form: NgForm){
-		console.log(form.value.answerDescription);
+		const answer = new Answer(
+			form.value.answerDescription,
+			this.question,
+			new Date(),
+			new User('Fernando','Lagunes')
+		);
+		console.log(answer.question);
+		//Agrego la respuesta que genere aqui a la propiedad answer de mi pregunta
+		this.question.answer.unshift(answer);
+		//Reseteo el formulario para que borre los datos despues de insertar la pregunta
+		form.reset();
 	}
 }
